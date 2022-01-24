310115a2927fb94f6cf48bf462c9ca4190262f4e
Dan Goldberg <dan@castlerockcs.com>
2022-01-24 17:24:27 -0500

    update args

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
+export GOROOT=/libpostal/go
+export GOPATH=/libpostal/workspace
+export PATH=$PATH:/libpostal/go/bin
 go get github.com/johnlonganecker/libpostal-rest
 go install github.com/johnlonganecker/libpostal-rest
