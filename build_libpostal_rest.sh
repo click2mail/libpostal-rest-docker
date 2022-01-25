526b503df12fe6dadeaf8fe391ecc14a9bcf737d
Dan Goldberg <dan@madjic.net>
2022-01-24 19:32:25 -0500

    clear vars

--- build_libpostal_rest.sh
+++ build_libpostal_rest.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -e
-export GOROOT=/libpostal/go
-export GOPATH=/libpostal/workspace
-export PATH=$PATH:/libpostal/go/bin
+
 go get github.com/johnlonganecker/libpostal-rest
 go install github.com/johnlonganecker/libpostal-rest
